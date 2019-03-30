8c1d4161adf4947f4fe2c016c30c1516c558b5ea
Tigran <tigrankhachikyan@gmail.com>
2019-03-30 22:59:30 +0400

    clone frontend project

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 git clone https://github.com/davellanedam/node-express-mongodb-jwt-rest-api-skeleton backend/node-express-mongodb-jwt-rest-api-skeleton
+git clone https://github.com/davellanedam/vue-skeleton-mvp frontend/vue-skeleton-mvp
