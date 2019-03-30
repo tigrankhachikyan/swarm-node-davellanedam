6aaa1e8d7708bb0e3ac33bb9c51aa2cccf002495
Tigran <tigrankhachikyan@gmail.com>
2019-03-30 23:01:06 +0400

    remove .git dirs after clone

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
+
+echo "Cloning sumbodule projects"
 git clone https://github.com/davellanedam/node-express-mongodb-jwt-rest-api-skeleton backend/node-express-mongodb-jwt-rest-api-skeleton
 git clone https://github.com/davellanedam/vue-skeleton-mvp frontend/vue-skeleton-mvp
+
+echo "remove .git directories from the submodules to keep track of local changes"
+rm -rf backend/node-express-mongodb-jwt-rest-api-skeleton/.git
+rm -rf frontend/vue-skeleton-mvp/.git
